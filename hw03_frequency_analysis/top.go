0cff278b73826cd9b02d09c36151d5c0178f1774
darya <babaikabarabaika@yandex.by>
2026-04-09 11:25:19 +0300

    3

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,43 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+func Top10(text string) []string {
+	words := strings.Fields(text)
+	resultMap := make(map[string]int)
+	for _, word := range words {
+		resultMap[word]++
+	}
+
+	type wordStatistic struct {
+		word  string
+		count int
+	}
+
+	wordStatistics := make([]wordStatistic, 0, len(resultMap))
+	for word, count := range resultMap {
+		wordStatistics = append(wordStatistics, wordStatistic{word, count})
+	}
+
+	sort.Slice(wordStatistics, func(i, j int) bool {
+		if wordStatistics[i].count == wordStatistics[j].count {
+			return wordStatistics[i].word < wordStatistics[j].word
+		}
+		return wordStatistics[i].count > wordStatistics[j].count
+	})
+
+	length := 10
+	if len(wordStatistics) < length {
+		length = len(wordStatistics)
+	}
+
+	result := make([]string, length)
+	for i := 0; i < length; i++ {
+		result[i] = wordStatistics[i].word
+	}
+
+	return result
 }
